a90d081235d7bf581955d7c7938c7fdf04506ca7
omogbolahan01 <iloriojommodel@gmail.com>
2023-10-06 20:10:37 +0100

    correcting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { BsCartCheck } from "react-icons/Bs";
+import { BsCartCheck } from "react-icons/bs";
 import { useCart } from "../pages/CartContext";
 import CartView from "../pages/CartView";
 
