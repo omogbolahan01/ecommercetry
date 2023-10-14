db50875d0c767c63856f80ec86a1d7bbd67eb945
omogbolahan01 <iloriojommodel@gmail.com>
2023-10-14 19:12:43 +0100

    going

--- src/pages/CartView.jsx
+++ src/pages/CartView.jsx
@@ -28,14 +28,10 @@ const CartView = (catalogue) => {
     setCartVisibility(!isCartVisible);
   };
   const checkout = async () => {
-    // In a real application, this function would integrate with a payment gateway
-    // and process the payment. Here, we'll simulate a successful payment.
-    const paymentResult = await mockPayment(); // Simulated payment
+    const paymentResult = await mockPayment();
     if (paymentResult === "success") {
-      // Successful payment, proceed with order confirmation and clearing the cart
       alert("Payment successful! Your order has been confirmed.");
-      // TODO: Implement order confirmation and clear the cart here
-      // For simplicity, we'll just clear the cart here.
+
       cancelCart();
     } else {
       alert("Payment failed. Please try again.");
@@ -44,18 +40,12 @@ const CartView = (catalogue) => {
 
   const mockPayment = () => {
     return new Promise((resolve) => {
-      // Simulate payment success after a delay
       setTimeout(() => {
-        resolve("success"); // In a real app, this would depend on the payment gateway response
+        resolve("success");
       }, 2000);
     });
   };
-  // const formattedSum = () =>
-  //   calculateTotal.toLocaleString("en-US", {
-  //     style: "decimal",
-  //     minimumFractionDigits: 2,
-  //     maximumFractionDigits: 2,
-  //   });
+
   return (
     <div className="cart-view" onClick={handleClickInsideCart}>
       <h2>Cart</h2>
